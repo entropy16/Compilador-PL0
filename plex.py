7ea5ab1b997053b71c49eec960005e084883e8f9
Sebastian Narvaez Jaramillo <78891149+entropy16@users.noreply.github.com>
2023-10-26 14:45:47 -0500

    Update plex.py

--- plex.py
+++ plex.py
@@ -11,7 +11,7 @@ class pl0Lexer(Lexer):
         FUN, INT_TYPE, FLOAT_TYPE, STRING_TYPE,
 
         # Operador de asignación
-        ASIGN,
+        ASSIGN,
         # Operadores lógicos
         NOT, AND, OR, 
 
